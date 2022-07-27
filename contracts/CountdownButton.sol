9d3dae999fcc33e62c227ca07dc77e3d9f2d84b7
anisha256 <1aanisha.rai@gmail.com>
2022-07-28 01:46:56 +0545

    reset countdown

--- contracts/CountdownButton.sol
+++ contracts/CountdownButton.sol
@@ -20,7 +20,7 @@ contract CountdownButton {
         uint256 pId;
         address pAddress;
         uint256 balance;
-        // uint256 joinAt;
+        uint256 joinAt;
         bool choice;
         bool isLeader;
     }
@@ -30,21 +30,20 @@ contract CountdownButton {
     address public participantAddress;
     address public winnerAddress;
     uint256 public count;
+    uint256 public totalPrize;
 
     uint256 public unlockPeriod;
-    uint256 public startAt;
-    uint256 public endAt;
+    uint256 public countdownStart;
+    uint256 public countdownEnd;
 
-    uint256 public totalPrize;
+    bool public reset;
+    uint256 public newCountdownEnd;
 
     //mapping
     mapping(uint256 => Participant) public participants;
     // mapping(address => Participant) public participants;
 
-    
-
     //modifiers
-  
     modifier inState(State _state) {
         require(state == _state);
         _;
@@ -54,75 +53,64 @@ contract CountdownButton {
     event Create(uint256 pcount);
 
     //functions
-    constructor(TestToken _token, uint256 _unluckPeriod, uint256 _startAt) {
+    constructor(
+        TestToken _token,
+        uint256 _unluckPeriod,
+        uint256 _startAt
+    ) {
         token = _token;
         unlockPeriod = _unluckPeriod;
-        startAt = _startAt;
-        endAt = unlockPeriod.add(startAt);
+        countdownStart = _startAt;
+        countdownEnd = unlockPeriod.add(countdownStart);
         state = State.START;
     }
 
-    function onClick(bool _choice)
-        public
-        inState(State.START)
-    {
-
+    function onClick(bool _choice) public inState(State.START) {
         participantAddress = msg.sender;
-        require(
-            block.timestamp < endAt,
-            "CANNOT PARTICIPATE"
-        );
-            count += 1;
+        require(block.timestamp < countdownEnd, "CANNOT PARTICIPATE");
+        count += 1;
 
         if (_choice) {
+            // participants[count] = Participant({
+            //     pId: count,
+            //     pAddress: msg.sender,
+            //     balance: token.balanceOf(msg.sender),
+            //     joinAt: block.timestamp,
+            //     choice: _choice,
+            //     isLeader: true
+            // });
+            Participant storage p = participants[count];
+            p.pId = count;
+            p.pAddress = msg.sender;
+            p.balance = token.balanceOf(msg.sender);
+            p.joinAt = block.timestamp;
+            p.choice = _choice;
+            p.isLeader = true;
 
-            participants[count] = Participant({
-                pId:count,
-                pAddress: msg.sender,
-                balance: token.balanceOf(msg.sender),
-                // joinAt: _joinAt,
-                choice: _choice,
-                isLeader: true
-            });
+            token.transferFrom(msg.sender, address(this), 20000000000000000000);
 
-            token.transferFrom(
-                msg.sender,
-                address(this),
-                20000000000000000000
-            );
-            
-        emit Create(count);
+            reset = true;
+            newCountdownEnd = p.joinAt.add(countdownEnd.sub(p.joinAt));
+            emit Create(count);
         }
-            // state = State.COUNTDOWN;
-
-    }
-    function getCountdown() 
-    public 
-    view
-    /** inState(State.COUNTDOWN) */
-    returns(uint256){
-        require(block.timestamp <= endAt,"Countdown is over");
-        // state = State.COUNTDOWN_EXPIRED;
-        return endAt.sub(block.timestamp);
+        // state = State.COUNTDOWN;
     }
 
-    function winner() public 
-    /**inState(State.COUNTDOWN_EXPIRED)  */ 
+    function getCountdown() public view returns (uint256) {
+        require(block.timestamp <= newCountdownEnd, "Countdown is over");
+        return newCountdownEnd.sub(block.timestamp);
+    }
+
+    function winner() public /**inState(State.COUNTDOWN_EXPIRED)  */
     {
-        require(
-            block.timestamp >= endAt,
-            " REWARDS IS LOCKED"
-        );
+        require(block.timestamp >= newCountdownEnd, " REWARDS IS LOCKED");
         //declare winner
         Participant memory p = participants[count];
-        if(p.isLeader){
-        winnerAddress = p.pAddress;
+        if (p.isLeader) {
+            winnerAddress = p.pAddress;
             //withdraw the price
-        totalPrize = token.balanceOf(address(this));
-        token.transfer(winnerAddress, totalPrize);
+            totalPrize = token.balanceOf(address(this));
+            token.transfer(winnerAddress, totalPrize);
         }
-     
     }
-
-   
 }
